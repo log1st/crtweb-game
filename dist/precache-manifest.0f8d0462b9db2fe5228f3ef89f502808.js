bfaa38c3e268767dbdb3824af6628f7c1c1c03b6
sp <sp@solarcom.ch>
2021-10-05 01:28:52 +0300

    build

---
+++ dist/precache-manifest.0f8d0462b9db2fe5228f3ef89f502808.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804e0ed525b5144dae6b",
+    "url": "/css/app.52368e55.css"
+  },
+  {
+    "revision": "073f465990d82716c7fd",
+    "url": "/css/results.ffa3f4f2.css"
+  },
+  {
+    "revision": "d00c685252e55bf3d80a783c29260d26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "804e0ed525b5144dae6b",
+    "url": "/js/app.4adc877c.js"
+  },
+  {
+    "revision": "d25149075182dc059e1d",
+    "url": "/js/chunk-vendors.f30d665a.js"
+  },
+  {
+    "revision": "073f465990d82716c7fd",
+    "url": "/js/results.081af452.js"
+  },
+  {
+    "revision": "5ceefedf457383a2accf863c24de2b96",
+    "url": "/manifest.json"
+  }
+]);
