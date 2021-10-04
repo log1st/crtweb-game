eeebf21b7523dff73bb00508fbe0b69f9de889c4
sp <sp@solarcom.ch>
2021-10-05 01:32:20 +0300

    -

---
+++ docs/precache-manifest.6f4bd0e092774db87016624bc849843f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50a7fb2dbbdb366559c",
+    "url": "/crtweb-game/css/app.52368e55.css"
+  },
+  {
+    "revision": "073f465990d82716c7fd",
+    "url": "/crtweb-game/css/results.ffa3f4f2.css"
+  },
+  {
+    "revision": "b5a2da4e962ee4fece0bc2e4301ebb64",
+    "url": "/crtweb-game/index.html"
+  },
+  {
+    "revision": "a50a7fb2dbbdb366559c",
+    "url": "/crtweb-game/js/app.bc105b95.js"
+  },
+  {
+    "revision": "d25149075182dc059e1d",
+    "url": "/crtweb-game/js/chunk-vendors.f30d665a.js"
+  },
+  {
+    "revision": "073f465990d82716c7fd",
+    "url": "/crtweb-game/js/results.081af452.js"
+  },
+  {
+    "revision": "5ceefedf457383a2accf863c24de2b96",
+    "url": "/crtweb-game/manifest.json"
+  }
+]);
